3b72385d326251577bc7cf9c89a223e985c5c9be
tjhillard <tjhillard@gmail.com>
2018-09-02 17:41:08 -0700

    add side navigation for mobile

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="navbar-wrapper">
-    <v-layout>
+    <v-layout wrap>
       <v-flex md10>
         <v-toolbar flat color="white">
+          <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
           <router-link tag="v-toolbar-title" to="/" class="link">Brewdog</router-link>
           <v-spacer></v-spacer>
           <v-toolbar-items class="hidden-sm-and-down">
@@ -13,6 +14,38 @@
           </v-toolbar-items>
         </v-toolbar>
       </v-flex>
+       <v-navigation-drawer
+          v-model="drawer"
+          enable-resize-watcher
+          light
+          app
+          >
+          <v-list class="pa-1">
+            <v-list-tile>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  <h2>Brewdog</h2>
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <router-link
+              :to="item.href"
+              tag="v-list-tile"
+              v-for="item in navDrawerItems"
+              :key="item.title"
+            >
+              <v-list-tile-action right>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              </v-list-tile-content>
+            </router-link>
+          </v-list>
+          <v-list class="pt-0" dense>
+            <v-divider></v-divider>
+          </v-list>
+        </v-navigation-drawer>
       <v-flex md2 class="nav-search-wrapper">
         <v-text-field
             label="Solo"
@@ -36,7 +69,28 @@ export default {
   data() {
     return {
       searchQuery: '',
+      drawer: false,
+      navDrawerItems: [
+        { title: 'Featured', icon: 'star', href: '/featured' },
+        { title: 'Beer Galery', icon: 'grid_on', href: '/gallery' },
+        { title: 'Ingredients', icon: 'local_dining', href: '/ingredients' },
+        { title: 'About', icon: 'help', href: '/about' },
+      ],
     };
+  },
+  mounted() {
+    this.onResize();
+    window.addEventListener('resize', this.onResize, { passive: true });
+  },
+  beforeDestroy() {
+    if (typeof window !== 'undefined') {
+      window.removeEventListener('resize', this.onResize, { passive: true });
+    }
+  },
+  methods: {
+    onResize() {
+      this.isMobile = window.innerWidth < 700;
+    },
   },
   watch: {
     searchQuery(newVal) {
@@ -59,12 +113,28 @@ export default {
 .link {
   cursor: pointer;
 }
+@media (min-width: 959px) {
+  .nav-search {
+    height: 50px;
+    padding-top: 7px;
+    padding-left: 10px;
+  }
+}
+
+@media (max-width: 959px) {
+  .v-text-field {
+    max-height: 35px !important;
+  }
+}
 .nav-search {
-  height: 50px;
-  padding-top: 7px;
+  margin-top: 7px;
   padding-left: 10px;
+  height: 50px;
 }
 .nav-search-wrapper {
+  @media (max-width: 960px) {
+    height: 60px;
+  }
   border-left: 1px solid lightgrey;
 }
 </style>
